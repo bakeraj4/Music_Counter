b92f44377797e28c0697bda07bcb0d37017315ca
Aaron Baker <bakeraj4@miamioh.edu>
2014-01-27 08:55:24 -0500

    Figuring out how to save the graphics.

--- src/FrameImage.java
+++ src/FrameImage.java
@@ -1,9 +1,13 @@
 import java.awt.*;  
 import java.awt.event.*;  
 import java.awt.image.BufferedImage;  
+import java.awt.image.RenderedImage;
 import java.io.*;  
 import java.net.*;  
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.imageio.ImageIO;  
    
@@ -35,23 +39,35 @@ public class FrameImage{
 	    }  
 	   
 	    public void paint(Graphics g){  
-	        super.paint(g); 
+	        super.paint(g);
 	        int x = (getWidth() - image[0].getWidth())/2;  
 	        int y = (getHeight() - image[0].getHeight())/2;  
 	        g.drawImage(image[0], x, y, this); 
 	        //TODO this method will need to change to handle the array list of buffered images and places them in the correct area (some type of loop and place in the spiral in reverse such that the ones toward the front are on top of the previuos ones)
+	 
+	        //save the image
+	        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+	        File results=new File("RESULTS_"+dateFormat.format(new Date()).replace('/', '_')+".jpg");
 	        
-	        //TODO need to save the image
-	    }  
+	        //this.getGraphics()
+	        //this.getGraphicsConfiguration()
+	       
+	        
+	    }
 	   
 	    public Dimension getPreferredSize(){//TODO will need to change image[i] and the parameter of i 
 	        return new Dimension(image[0].getWidth(), image[0].getHeight());  
 	    }  
 	   
-	    private void loadImage(){//TODO a for loop for the arraylist of url srings [i]
+	    private void loadImage(){
 	        try{  
-	            URL url = new URL(myURLs.get(0));//("https://scontent-b-iad.xx.fbcdn.net/hphotos-prn2/1384034_745841162097861_1937544369_n.jpg");  
-	            image[0] = ImageIO.read(url);  
+	            URL url; 
+	            int i=0;
+	            for(String bands:myURLs){
+	            	url = new URL(bands);
+	            	image[i] = ImageIO.read(url); 
+		            i++;
+	            }
 	        }catch(Exception e){
 	        }
 	    }  
